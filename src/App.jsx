711668afbf1ee13ed372a95b9c4ee968252ff653
koray.ozdemir <koray.ozdemir11@gmail.com>
2024-10-09 21:20:57 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+function App() {
+  return (
+    <div className="container mx-auto p-28">
+      <div className="relative flex">
+          <div className="h-[468px] w-[218px] border-[#d300c5] border-2 rounded-[30px] rotate-[-10deg] mr-[-68px] mt-[-50px]  ">
+          <img src="/src/assets/phone1/screenshot2-2x.png" alt="" className="w-full h-full rounded-[30px] "/>
+          </div>
+          <div className="h-[468px] w-[218px] border-[#d300c5] border-2 rounded-[30px] rotate-[8deg]  ">
+          <img src="/src/assets/phone1/screenshot2-2x.png" alt="" className="w-full h-full rounded-[30px] "/>
+          </div>
+      </div>
+      <div>
+
+      </div>
+    </div>
+  )
+}
+
+export default App
