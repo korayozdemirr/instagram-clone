6bea93705b0391afc8d7c20a08b02ce99914cd90
Koray Özdemir <korayozdemir@192.168.1.101>
2024-10-20 22:39:10 +0300

    home sidebar

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -7,70 +7,80 @@ function SideBar() {
       <div className="mb-7 mt-5 p-3">
         <img src="/src/assets/logo.png" alt="instagram" className="w-[105px]" />
       </div>
-      <NavLink to="/">
-        {({ isActive }) => (
-          <div className="flex items-center gap-x-4 p-3 rounded hover:bg-zinc-200">
-            <Icon name={isActive ? "home-filled" : "home"} size={24} />
-            <span className="text-base font-bold">Ana Sayfa</span>
-          </div>
-        )}
-      </NavLink>
-      <NavLink to="#">
-        {({ isActive }) => (
-          <div className="flex items-center gap-x-4 p-3 rounded hover:bg-zinc-200">
-            <Icon name={isActive ? "search" : ""} size={24} />
-            <span className="text-base font-normal">Ara</span>
-          </div>
-        )}
-      </NavLink>
-      <NavLink to="/explore">
-        {({ isActive }) => (
-          <div className="flex items-center gap-x-4 p-3 rounded hover:bg-zinc-200">
-            <Icon name={isActive ? "explore" : "explore"} size={24} />
-            <span className="text-base">Keşfet</span>
-          </div>
-        )}
-      </NavLink>
-      <NavLink to="/reels">
-        {({ isActive }) => (
-          <div className="flex items-center gap-x-4 p-3 rounded hover:bg-zinc-200">
-            <Icon name={isActive ? "reels" : "reels"} size={24} />
-            <span className="text-base">Reels</span>
-          </div>
-        )}
-      </NavLink>
-      <NavLink to="/inbox">
-        {({ isActive }) => (
-          <div className="flex items-center gap-x-4 p-3 rounded hover:bg-zinc-200">
-            <Icon name={isActive ? "direct-filled" : "direct"} size={24} />
-            <span className="text-base">Mesajlar</span>
-          </div>
-        )}
-      </NavLink>
-      <NavLink to="/#">
-        {({ isActive }) => (
-          <div className="flex items-center gap-x-4 p-3 rounded hover:bg-zinc-200">
-            <Icon name={isActive ? "heart" : "heart"} size={24} />
-            <span className="text-base">Bildirimler</span>
-          </div>
-        )}
-      </NavLink>
-      <NavLink to="/#">
-        {({ isActive }) => (
-          <div className="flex items-center gap-x-4 p-3 rounded hover:bg-zinc-200">
-            <Icon name={isActive ? "create" : "create"} size={24} />
-            <span className="text-base">Oluştur</span>
-          </div>
-        )}
-      </NavLink>
-      <NavLink to="/profile">
-        {({ isActive }) => (
-          <div className="flex items-center gap-x-4 p-3 rounded hover:bg-zinc-200">
-            <img src="/no-avatar.jpeg" alt="" className="rounded-full w-[28px] h-[28px]" />
-            <span className="text-base">Profil</span>
-          </div>
-        )}
-      </NavLink>
+      <nav className="flex flex-col gap-y-2 mb-10">
+        <NavLink to="/">
+          {({ isActive }) => (
+            <div className="flex items-center gap-x-4 p-3 rounded hover:bg-zinc-200">
+              <Icon name={isActive ? "home-filled" : "home"} size={24} />
+              <span className="text-base font-bold">Ana Sayfa</span>
+            </div>
+          )}
+        </NavLink>
+        <NavLink to="#">
+          {({ isActive }) => (
+            <div className="flex items-center gap-x-4 p-3 rounded hover:bg-zinc-200">
+              <Icon name={isActive ? "search" : ""} size={24} />
+              <span className="text-base font-normal">Ara</span>
+            </div>
+          )}
+        </NavLink>
+        <NavLink to="/explore">
+          {({ isActive }) => (
+            <div className="flex items-center gap-x-4 p-3 rounded hover:bg-zinc-200">
+              <Icon name={isActive ? "explore" : "explore"} size={24} />
+              <span className="text-base">Keşfet</span>
+            </div>
+          )}
+        </NavLink>
+        <NavLink to="/reels">
+          {({ isActive }) => (
+            <div className="flex items-center gap-x-4 p-3 rounded hover:bg-zinc-200">
+              <Icon name={isActive ? "reels" : "reels"} size={24} />
+              <span className="text-base">Reels</span>
+            </div>
+          )}
+        </NavLink>
+        <NavLink to="/inbox">
+          {({ isActive }) => (
+            <div className="flex items-center gap-x-4 p-3 rounded hover:bg-zinc-200">
+              <Icon name={isActive ? "direct-filled" : "direct"} size={24} />
+              <span className="text-base">Mesajlar</span>
+            </div>
+          )}
+        </NavLink>
+        <NavLink to="/#">
+          {({ isActive }) => (
+            <div className="flex items-center gap-x-4 p-3 rounded hover:bg-zinc-200">
+              <Icon name={isActive ? "heart" : "heart"} size={24} />
+              <span className="text-base">Bildirimler</span>
+            </div>
+          )}
+        </NavLink>
+        <NavLink to="/#">
+          {({ isActive }) => (
+            <div className="flex items-center gap-x-4 p-3 rounded hover:bg-zinc-200">
+              <Icon name={isActive ? "create" : "create"} size={24} />
+              <span className="text-base">Oluştur</span>
+            </div>
+          )}
+        </NavLink>
+        <NavLink to="/profile">
+          {({ isActive }) => (
+            <div className="flex items-center gap-x-4 p-3 rounded hover:bg-zinc-200">
+              <img
+                src="/no-avatar.jpeg"
+                alt=""
+                className="rounded-full w-[28px] h-[28px]"
+              />
+              <span className="text-base">Profil</span>
+            </div>
+          )}
+        </NavLink>
+      </nav>
+      <div className="flex gap-x-4 p-3">
+        <Icon name="hamburger" size={24} />
+        <p>Daha Fazlası</p>
+      </div>
     </div>
   );
 }
